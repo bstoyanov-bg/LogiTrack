8fbde6d42d949fcca87d5ea04bbcc959b20afaee
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-10-30 18:43:35 +0200

    Add EF Core initial migration with data annotations and update database.

---
+++ ShipmentService/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShipmentService.Models;
+
+namespace ShipmentService.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Shipment> Shipments => Set<Shipment>();
+}
---
+++ ShipmentService/Models/Shipment.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShipmentService.Models
+{
+    public enum ShipmentStatus
+    {
+        Created,
+        InTransit,
+        Delivered,
+        Cancelled
+    }
+
+    public class Shipment
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string TrackingNumber { get; set; } = null!;
+        [Required]
+        [MaxLength(100)]
+        public string Origin { get; set; } = null!;
+        [Required]
+        [MaxLength(100)]
+        public string Destination { get; set; } = null!;
+        [Required]
+        public ShipmentStatus Status { get; set; } = ShipmentStatus.Created;
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
