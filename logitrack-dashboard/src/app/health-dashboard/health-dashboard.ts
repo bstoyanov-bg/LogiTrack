0b274e706a0df7ae53acb009e7f6067f87fe50b2
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-11-06 17:01:22 +0200

    Set 10 seconds interval refresh

--- logitrack-dashboard/src/app/health-dashboard/health-dashboard.ts
+++ logitrack-dashboard/src/app/health-dashboard/health-dashboard.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HealthService } from '../services/health.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-health-dashboard',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './health-dashboard.html',
   styleUrls: ['./health-dashboard.scss']
 })
-export class HealthDashboardComponent implements OnInit {
+export class HealthDashboardComponent implements OnInit, OnDestroy {
+  private intervalId: any;
+
   constructor(public health: HealthService) {}
 
   ngOnInit() {
-    this.health.checkApi();
-    this.health.checkRedis();
+    this.refresh();
+    // Refresh on 10 seconds
+    this.intervalId = setInterval(() => this.refresh(), 10000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) clearInterval(this.intervalId);
   }
 
   refresh() {
