56946030e6dc95e41e72775a0baa9564475be4e1
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-11-07 15:29:06 +0200

    Make shipment table and driver table look the same.

--- logitrack-dashboard/src/app/components/drivers-dashboard/drivers-dashboard.ts
+++ logitrack-dashboard/src/app/components/drivers-dashboard/drivers-dashboard.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Driver, DriverService } from '../../services/driver.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @Component({
   selector: 'app-drivers-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatTableModule,
+    MatProgressSpinnerModule],
   templateUrl: './drivers-dashboard.html'
 })
 export class DriversDashboardComponent implements OnInit {
   drivers: Driver[] = [];
   loading = false;
-  
+
+  displayedColumns = ['id', 'name', 'status'];
+
   constructor(private driverService: DriverService) {}
 
   ngOnInit() {
